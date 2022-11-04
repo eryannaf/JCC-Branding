3a975b99251592601ed03940c34ddb5cd377bab1
eryannaf <eryan.fajriani@gmail.com>
2022-11-04 22:59:18 +0700

    first commit

---
+++ app/Http/Controllers/BridgeClassesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BridgeClasses;
+use Illuminate\Http\Request;
+
+class BridgeClassesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BridgeClasses  $bridgeClasses
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BridgeClasses $bridgeClasses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BridgeClasses  $bridgeClasses
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BridgeClasses $bridgeClasses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BridgeClasses  $bridgeClasses
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BridgeClasses $bridgeClasses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BridgeClasses  $bridgeClasses
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BridgeClasses $bridgeClasses)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TransactionHistoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TransactionHistories;
+use Illuminate\Http\Request;
+
+class TransactionHistoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TransactionHistories  $transactionHistories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TransactionHistories $transactionHistories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TransactionHistories  $transactionHistories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TransactionHistories $transactionHistories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TransactionHistories  $transactionHistories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TransactionHistories $transactionHistories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TransactionHistories  $transactionHistories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TransactionHistories $transactionHistories)
+    {
+        //
+    }
+}
